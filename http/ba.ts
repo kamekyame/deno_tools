ef2c06cb421ff1ca7501a0229f74beab043875b9
Suzu Tomo <kamekyame@outlook.com>
2022-04-03 18:45:48 +0900

    fix: first argument of fetch function deprecated URL object

--- http/ba.ts
+++ http/ba.ts
@@ -8,22 +8,29 @@ export interface BasicAuthInfo {
 const getBa = (auth: BasicAuthInfo): string =>
   `Basic ${Base64.fromString(`${auth.username}:${auth.password}`).toString()}`;
 
+interface BaFetch {
+  (
+    input: string | Request,
+    auth: BasicAuthInfo,
+    init?: RequestInit | undefined,
+  ): Promise<Response>;
+  /** @deprecated
+   * URL is deprecated as the first argument. Use string or Request object instead.
+   */
+  (
+    input: URL,
+    auth: BasicAuthInfo,
+    init?: RequestInit | undefined,
+  ): Promise<Response>;
+}
+
 /**
  * Fetch with basic auth
- *
- * @export
- * @param {(string | Request | URL)} input
- * @param {BasicAuthInfo} auth
- * @param {(RequestInit | undefined)} [init]
- * @return {Promise<Response>}
  */
-export function baFetch(
-  input: string | Request | URL,
-  auth: BasicAuthInfo,
-  init?: RequestInit | undefined,
-): Promise<Response> {
+export const baFetch: BaFetch = (input, auth, init?) => {
   const headers: HeadersInit = new Headers(init?.headers);
   headers.append("Authorization", getBa(auth));
 
-  return fetch(input, { ...init, headers: headers });
-}
+  if (input instanceof URL) return fetch(input, { ...init, headers: headers });
+  else return fetch(input, { ...init, headers: headers });
+};
