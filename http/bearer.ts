04cdbad0d7cc04a4c5f8b796e9f56ca34023b83e
SuzuTomo <47011206+kamekyame@users.noreply.github.com>
2022-04-03 18:58:04 +0900

    fix: first argument of bearerFetch deprecated URL object (#4)

--- http/bearer.ts
+++ http/bearer.ts
@@ -1,11 +1,24 @@
+interface BearerFetch {
+  (
+    input: string | Request,
+    bearerToken: string,
+    init?: RequestInit | undefined,
+  ): Promise<Response>;
+  /** @deprecated
+   * URL is deprecated as the first argument. Use string or Request object instead.
+   */
+  (
+    input: URL,
+    bearerToken: string,
+    init?: RequestInit | undefined,
+  ): Promise<Response>;
+}
+
 /** Fetch with bearer auth */
-export function bearerFetch(
-  input: string | Request | URL,
-  bearerToken: string,
-  init?: RequestInit | undefined,
-): Promise<Response> {
+export const bearerFetch: BearerFetch = (input, bearerToken, init?) => {
   const headers: HeadersInit = new Headers(init?.headers);
   headers.append("Authorization", `Bearer ${bearerToken}`);
 
-  return fetch(input, { ...init, headers: headers });
-}
+  if (input instanceof URL) return fetch(input, { ...init, headers: headers });
+  else return fetch(input, { ...init, headers: headers });
+};
