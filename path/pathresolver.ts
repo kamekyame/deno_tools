c48361ebd7473bc096504800a366257a28653096
denopendabot <denopendabot@github.com>
2024-01-08 00:52:56 +0000

    build(deps): bump std from 0.159.0 to 0.211.0

--- path/pathresolver.ts
+++ path/pathresolver.ts
@@ -1,4 +1,4 @@
-import * as path from "https://deno.land/std@0.159.0/path/mod.ts";
+import * as path from "https://deno.land/std@0.211.0/path/mod.ts";
 
 export function pathResolver(meta: ImportMeta): (p: string) => string {
   return (p) => path.fromFileUrl(new URL(p, meta.url));
